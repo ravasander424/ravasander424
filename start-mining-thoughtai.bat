2be77ff7b244fc3d3ea72f4ed5fc1ff83e0ebcd5
ravasander424 <tasty.outworn-79@icloud.com>
2025-11-22 16:12:33 +0100

    Add files via upload

---
+++ start-mining-thoughtai.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --algorithm tht --pool tht.mine-n-krush.org:5001 --wallet thoughtai-wallet --password d=1.0
+pause
